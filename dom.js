ac1cda754024e788dcbe8ffa616d44e447424382
muwaffaqnabdel <abdellmuwaffaq@gmail.com>
2024-11-15 00:14:02 +0700

    update data

---
+++ dom.js
@@ -0,0 +1,8 @@
+const a = document.querySelector('section#a a')
+a.setAttribute('href','"https://www.instagram.com/abdellmuwaffaq_"')
+
+//untuk contact//
+const btnSubmit = document.getElementById('btnSubmit')
+function alertSubmit(){
+    alert('terimakasih atas sarannya')
+}
